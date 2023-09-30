5d9d3580b3303ba6ec03eb953a5eed1e6abc32cb
dvip1 <dvip.d.patel@slrtce.in>
2023-09-30 17:02:06 +0530

    init

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (
+        <>
+            {/* Shaunak Edit here, use tailwind
+            You can write html here, remeber to use className instead of class
+        here's the documentation: https://tailwindcss.com/
+     for example if you need border radius you search for border-radius and you'll get the class name
+    or  give this prompt to chatgpt -> Tell me basic things about tailwind css to get started. 
+        */}
+    <h1 className="text-6xl">
+        Hello World
+    </h1>
+
+        </>
+    )
+}
